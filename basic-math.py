15d750c2ffa444d1b5633ce786751a36284cb4cc
açi <88833979+acelyabzn@users.noreply.github.com>
2022-06-22 10:03:59 +0300

    Add files via upload

---
+++ basic-math.py
@@ -0,0 +1,48 @@
+# + --> toplama
+# - --> çıkarma
+# * --> çarpma
+# / --> bölme
+# **--> üs 
+# % --> mod 
+# //--> tam bölme
+
+print(3+4)
+
+print (3.2+4)
+
+print(2*4)
+
+print(10/5) # bölme işleminde her zaman sonuç tam olsada değer float olarak gösterilir.
+
+print(3+5*2)
+
+print((3+5)*2)
+
+print(3+5*2/2)
+
+print((3+5)*2/6)
+
+# parantez içerisindeki işlem her zaman öncelikli gelir.
+
+# 2 üssü 3
+print(2**3)
+
+# karekökünü almış oluyoruz.
+print(25**0.5)
+
+# 10 mod 3 yani 10'dan 3'ü bölüyoruz ve bize kalanı veriyor.
+print(10%3)
+
+print(10%4)
+
+# eğer bir sayıyı 2'ye bölüyorsak ve kalan 0 ise çift 1 ise tek sayıdır.
+
+# çift sayı.
+print(100%2)
+
+# tek sayı
+print(101%2)
+
+# print(10//3) normalde tam bölünmemektedir fakat "//"  operatörü tam bölmektedir. Sonuç her zaman integer.
+print(10//3)
+
---
+++ comment.py
@@ -0,0 +1,6 @@
+# Yorum satırı nasıl konulur bu dosyada bunu görüyoruz.
+# Yorum satırının kısaltması CTRL + K + C
+
+"""
+Bu şekkilde de yorum satırı yapılabilinir.
+"""
