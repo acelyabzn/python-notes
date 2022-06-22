ee640a3d801061d7eaa6fe6820ddad3da232c29c
açi <88833979+acelyabzn@users.noreply.github.com>
2022-06-22 13:12:17 +0300

    Add files via upload

---
+++ uygulama-1.py
@@ -0,0 +1,49 @@
+"""
+Uygulama 1: Bir öğrencinin aşağıdaki billgileri için gerekli değişkenleri oluşturunuz.
+1-Öğrenci adı
+2-Öğrenci soyadı
+3-Öğrenci ad + soyad
+4-Öğrenci numarası
+5-Öğrenci cinsiyet
+6-Öğrenci tc kimlik
+7-Öğrenci doğum yılı
+8-Öğrenci adres bilgisi
+9-Öğrenci yaşı
+"""
+
+# 1
+ad = "Açelya"
+print(ad)
+
+# 2
+soyad = "Bazan"
+print(soyad)
+
+# 3
+ad_soyad = ad +" "+ soyad
+print(ad_soyad)
+
+# 4
+numara = "123454"
+print(numara)
+
+# 5
+cinsiyet = True  # Erkek
+cinsiyet = False  # Kadın
+print(cinsiyet)
+
+# 6
+tc = "99887766554"
+print(tc)
+
+# 7
+dogum_yili = 2004
+print(dogum_yili)
+
+# 8
+adres = "Türkiye"
+print(adres)
+
+# 9
+yas = 2022 - dogum_yili
+print(yas)
