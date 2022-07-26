29715f44cb702bbb91c7e26322594d86075eef80
açi <88833979+acelyabzn@users.noreply.github.com>
2022-07-26 21:12:37 +0300

    Update uygulama-3.py

--- part-5/uygulama-3.py
+++ part-5/uygulama-3.py
@@ -1,4 +1,4 @@
-# 1- Girilen bir sayının 50-100 arasınnda olup olmadığını kontrol ediniz.
+# 1- Girilen bir sayının 50-100 arasında olup olmadığını kontrol ediniz.
 
 # 2- Girilen bir sayının pozitif tek sayı olup olmadığını kontrol ediniz.
 
@@ -20,16 +20,113 @@
 # 30.0 - 34.9  => Obez
 
 # 1. İstek:
-sayi_1 = int(input("1. Sayı:"))
+sayi1 = int(input("Sayı Giriniz:"))
 
-if (50 < sayi_1 and sayi_1 < 100):
-    print(f"{sayi_1} ~Sayı 50 ile 100 arasındadır")
+if (100 > sayi1 > 50):
+    print(f"{sayi1} 50 ile 100 arasındadır.")
 
-elif (sayi_1 < 50 and sayi_1 > 100):
-    print(f"{sayi_1} ~Sayı 50 ile 100 arasında değildir.")
+else:
+    print(f"{sayi1} 50 ile 100 arasında değildir.")
 
 # 2. İstek:
-sayi_2 = int(input("2. Sayı:"))
+sayi2 = int(input("Sayı Giriniz:"))
 
-if (sayi_2 > 0 and sayi_2 % 2 == 1):
-    print(f"{sayi_2} Sayısı tek ve pozitif bir sayıdır.")
+if (-1 < sayi2 and (sayi2 % 2 == 1)):
+    print(f"{sayi2} Pozitif ve Tek Sayıdır.")
+
+else:
+    print(f"{sayi2} Pozitif ve Tek Sayı Değildir.")
+
+# 3. İstek:
+
+username = "acelyabzn"
+password = "1234"
+
+type1 = str(input("Kullanıcı Adınızı Giriniz:"))
+type2 = input("Şifrenizi Giriniz:")
+
+if ((type1 == username) and (type2 == password)):
+    print("Kullanıcı Adı ve Parolanız Doğrudur.. Hoş Geldiniz")
+
+elif ((type1 != username) and (type2 == password)):
+    print("Kullanıcı Adınız Yanlıştır.. Tekrar Deneyiniz.")
+
+elif ((type1 == username) and (type2 != password)):
+    print("Parolanız Yanlıştır.. Tekrar Deneyiniz.")
+
+else:
+    print("Kullanıcı Adınız ve Parolanız Yanlıştır.. Tekrar Deneyiniz.")
+
+# 4. İstek:
+sayi3 = int(input("1. Sayı Giriniz:"))
+sayi4 = int(input("2. Sayı Giriniz:"))
+sayi5 = int(input("3. Sayı Giriniz:"))
+
+if (sayi3 > sayi4 > sayi5):
+    print(f"{sayi3} > {sayi4} > {sayi5}")
+
+elif (sayi5 > sayi4 > sayi3):
+    print(f"{sayi5} > {sayi4} > {sayi3}")
+
+elif (sayi4 > sayi5 > sayi3):
+    print(f"{sayi4} > {sayi5} > {sayi3}")
+
+elif (sayi3 > sayi5 > sayi4):
+    print(f"{sayi3} > {sayi5} > {sayi4}")
+
+elif (sayi5 > sayi3 > sayi4):
+    print(f"{sayi5} > {sayi4} > {sayi3}")
+
+elif (sayi5 > sayi3 > sayi4):
+    print(f"{sayi5} > {sayi4} > {sayi3}")
+
+else:
+    (print("iki veya üç tane sayı birbirine eşittir."))
+
+"""
+Birazdan Devam Edeceğim...
+"""
+
+"""
+Devamm...
+"""
+
+# 5. İstek:
+vize1 = int(input("1. Vize Notu: "))
+vize2 = int(input("2. Vize Notu: "))
+final = int(input("Final Notu: "))
+
+sonuc = ((((vize1 + vize2) / 2) * 0.6) + (final * 0.4))
+sonuc2 = ((((vize1 + vize2) / 2) * 0.6) + (final))
+
+if ((sonuc >= 50) and (final >= 70)):
+    print(f"{sonuc2} Geçerli Not Ortalaması. Final Notunuz 70 veya 70'in Üzerindedir")
+
+elif (sonuc >= 50):
+    print(f"{sonuc} Geçerli Not Ortalaması.")
+
+elif (sonuc <= 50):
+    print(f"{sonuc} Geçersiz Not Ortalaması.")
+
+# 6. İstek:
+name = str(input("Adınız: "))
+kilo = int(input("Kilonuz: "))
+height = float(input("Boy Uzunluğunuz: "))
+
+formül = (kilo / (height ** 2))
+
+if (0 <= formül <= 18.4):
+    print(f"{name} Vücut Inedxiniz {formül} (Zayıf) Kategorisindesiniz.")
+
+elif (18.5 <= formül <= 24.9):
+    print(f"{name} Vücut Inedxiniz {formül} (Normal) Kategorisindesiniz.")
+
+elif (25.0 <= formül <= 29.9):
+    print(f"{name} Vücut Inedxiniz {formül} (Fazla Kilolu) Kategorisindesiniz.")
+
+elif (30.0 <= formül <= 34.9):
+    print(f"{name} Vücut Inedxiniz {formül} (Obez) Kategorisindesiniz.")
+
+"""
+Koşul İfadelerine Geçmediğimiz İçin Sadık Hocam Farklı Yapmıştır.
+"""
